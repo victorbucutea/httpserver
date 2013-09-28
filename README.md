8b0074d0957acb2719925c82e0e72e2a6ec1af20
victor.bucutea@gmail.com <victor.bucutea@gmail.com>
2013-09-28 07:56:33 +0100

    keep alive support in launch

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 httpserver
 ==========
 
-multi thread http server
+Multi Thread Http File Server
+
+
+The server is a bare minimum http based file server. It supports download and upload (not yet multiplart/form-data) of files from the disk.
+
+To start the server simply build via <b>mvn clean install</b> and then just run <b> java.exe -jar target/http.server-1.0.jar </b>
+
+
+
--- src/main/java/com/adobe/server/Main.java
+++ src/main/java/com/adobe/server/Main.java
@@ -6,29 +6,32 @@ import java.io.InputStreamReader;
 
 import com.adobe.server.core.Server;
 
-
 public class Main {
 
 	static Logger logger = new Logger();
-	
+
 	public static void main(String[] args) throws IOException {
-		
+
 		int port = 80;
-		
-		if (args.length > 0 ){
+		boolean keepAlive = false;
+
+		if (args.length > 0) {
 			port = Integer.valueOf(args[0]);
+		} else if (args.length > 1) {
+			if ("keep-alive".equals(args[1]))
+				keepAlive = true;
 		}
-		
-		Server server = new Server(port);
+
+		Server server = new Server(port, keepAlive);
 		server.start();
-		
+
 		String command = "start";
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in) );
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		logger.log("--- To stop the server just type 'stop' followed by enter ---");
-		while (true){
+		while (true) {
 			command = br.readLine();
-			if (!"stop".equals(command)){
+			if (!"stop".equals(command)) {
 				logger.log("The only command is 'stop'. ");
 			} else {
 				server.stop();
